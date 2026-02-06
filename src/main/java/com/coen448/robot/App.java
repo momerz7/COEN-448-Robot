75152a170908ca1291820b14c9f788353e413c4c
{momerz7} <{didicr7@icloud.com}>
2026-02-06 13:21:50 -0500

    initial maven build

---
+++ src/main/java/com/coen448/robot/App.java
@@ -0,0 +1,13 @@
+package com.coen448.robot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
