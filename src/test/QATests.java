8d5fa8f72079e4caa2c05fd7a69d6313d7c41aa6
TylerLK <tylerLK2003@gmail.com>
2026-03-28 14:40:37 -0400

    Centralized QA Testing Class Created

---
+++ src/test/QATests.java
@@ -0,0 +1,6 @@
+public class QATests {
+    // This will hold all the QA testing methods.
+    // Files to copy from:
+    // src/test/java/com/coen448/robot/LKWQA.java
+    // src/test/java/com/coen448/robot/CoverageTestCasesTest.java
+}
