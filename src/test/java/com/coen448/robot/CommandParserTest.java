d228cac74c749595fc115207c8a516d9079dce96
Michael Iadisernia <michaelaiadisernia@gmail.com>
2026-04-07 20:14:05 -0400

    Critical logic leak fixed and added Move 0 test case to satisfy part of QA concerns.

--- src/test/java/com/coen448/robot/CommandParserTest.java
+++ src/test/java/com/coen448/robot/CommandParserTest.java
@@ -66,6 +66,10 @@ public class CommandParserTest {
     @Test
     void testMoveNegative(){ assertThrows(IllegalArgumentException.class, () -> parser.parse("M -5")); }
 
+    // added in response to QA team feedback about zero distance move command
+    @Test
+    void testMoveZero(){ assertThrows(IllegalArgumentException.class, () -> parser.parse("M 0")); }
+
     /* Test for commands */
 
     @Test
